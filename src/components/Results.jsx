875e32c453099c43666791cdb3d04590727e6b39
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-06-24 20:35:41 +0530

    images search done

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -10,8 +10,16 @@ export const Results = () => {
   const location = useLocation();
 
   useEffect(() => {
-    getResults("/search/q=JavaScript&num=40");
-  }, []);
+    if (searchTerm) {
+      if (location.pathname === "/videos") {
+        getResults(`/search/q=${searchTerm} videos`);
+      } else if (location.pathname === "/images") {
+        getResults(`/image/q=${searchTerm}&num=40`);
+      } else {
+        getResults(`${location.pathname}/q=${searchTerm}&num=40`);
+      }
+    }
+  }, [searchTerm, location.pathname]);
 
   if (isLoading) return <Loading />;
 
@@ -34,7 +42,24 @@ export const Results = () => {
         </div>
       );
     case "/images":
-      return "SEARCH";
+      return (
+        <div className="flex flex-wrap justify-center items-center">
+          {results?.image_results?.map(
+            ({ image, link: { href, title } }, i) => (
+              <a
+                href={href}
+                key={i}
+                target="_blank"
+                rel="noreferrer"
+                className="sm:p-3 p-5"
+              >
+                <img src={image?.src} alt={title} loading="lazy" />
+                <p className="w-36 break-words text-sm mt-2">{title}</p>
+              </a>
+            )
+          )}
+        </div>
+      );
     case "/news":
       return "SEARCH";
     case "/videos":
--- src/contexts/ResultContextProvider.js
+++ src/contexts/ResultContextProvider.js
@@ -6,7 +6,7 @@ const baseURL = "https://google-search3.p.rapidapi.com/api/v1";
 export const ResultContextProvider = ({ children }) => {
   const [results, setResults] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState("JavaScript");
 
   const getResults = async (type) => {
     setIsLoading(true);
