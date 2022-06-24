4f70698205acc212c888bc50fb75eac262212aa8
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-06-24 14:30:29 +0530

    required components created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { Navbar } from "./components/Navbar";
+import { Footer } from "./components/Footer";
+import { Routes } from "./components/Routes";
 
 const App = () => {
+  const [darkTheme, setDarkTheme] = useState(false);
+
   return (
     <div>
       <h1 className="text-3xl font-bold underline">App</h1>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <div>
+      <h1>Navbar</h1>
+    </div>
+  );
+};
