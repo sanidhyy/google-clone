49adb2e59cf8b758ec59733403bf508a7504a0e5
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-06-24 14:26:45 +0530

    tailwindcss setup

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <div>
-      <h1>App</h1>
+      <h1 className="text-3xl font-bold underline">App</h1>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App";
 
+import "./global.css";
+
 createRoot(document.getElementById("root")).render(<App />);
