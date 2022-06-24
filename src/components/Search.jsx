44f8b31929038865a980a0638855545c8ac96008
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-06-24 21:45:26 +0530

    search issues fixed

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,32 +5,34 @@ import { useResultContext } from "../contexts/ResultContextProvider";
 import { Links } from "./Links";
 
 export const Search = () => {
-  const [text, setText] = useState("Elon Musk");
+  const [search, setSearch] = useState("Elon Musk");
   const { setSearchTerm } = useResultContext();
-  const [debouncedValue] = useDebounce(text, 300);
 
-  useEffect(() => {
-    if (debouncedValue) setSearchTerm(debouncedValue);
-  }, [debouncedValue]);
+  const handleSearch = () => {
+    if (search.trim().length !== 0) {
+      setSearchTerm(search);
+    }
+  };
 
   return (
     <div className="relative sm:ml-48 md:ml-72 sm:-mt-10 mt-3">
       <input
         type="text"
-        value={text}
+        value={search}
+        onKeyDown={(e) => {
+          e.key === "Enter" && handleSearch();
+        }}
         placeholder="Seach Googl or type URL"
-        onChange={(e) => setText(e.target.value)}
+        onChange={(e) => setSearch(e.target.value)}
         className="sm:w-96 w-80 h-10 dark:bg-gray-200 border rounded-full shadow-sm outline-none p-6 text-black hover:shadow-lg"
       />
-      {text && (
-        <button
-          type="button"
-          className="absolute top-1.5 right-4 text-2xl text-gray-500"
-          onClick={() => setText("")}
-        >
-          X
-        </button>
-      )}
+      <button
+        type="button"
+        className="absolute top-1.5 right-4 text-2xl text-gray-500"
+        onClick={handleSearch}
+      >
+        🔎
+      </button>
       <Links />
     </div>
   );
--- src/contexts/ResultContextProvider.js
+++ src/contexts/ResultContextProvider.js
@@ -6,7 +6,7 @@ const baseURL = "https://google-search3.p.rapidapi.com/api/v1";
 export const ResultContextProvider = ({ children }) => {
   const [results, setResults] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [searchTerm, setSearchTerm] = useState("JavaScript");
+  const [searchTerm, setSearchTerm] = useState("Elon Musk");
 
   const getResults = async (type) => {
     setIsLoading(true);
