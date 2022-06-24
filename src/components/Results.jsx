7c7976f5c30b7ee22f48334d14972fe200067dcd
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-06-24 20:56:08 +0530

    videos search created

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -83,7 +83,20 @@ export const Results = () => {
         </div>
       );
     case "/videos":
-      return "SEARCH";
+      return (
+        <div className="flex flex-wrap">
+          {results?.map((video, i) => (
+            <div key={i} className="p-2">
+              <ReactPlayer
+                url={video.additional_links?.[0].href}
+                controls
+                width="355px"
+                height="200px"
+              />
+            </div>
+          ))}
+        </div>
+      );
     default:
       return "ERROR!";
   }
