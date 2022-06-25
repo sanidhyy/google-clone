5ba0387d26292238dfda837b0912e10ae245fb72
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-06-25 14:03:58 +0530

    fixed a typo 🙂

--- src/contexts/ResultContextProvider.js
+++ src/contexts/ResultContextProvider.js
@@ -10,7 +10,7 @@ const baseURL = "https://google-search3.p.rapidapi.com/api/v1";
 export const ResultContextProvider = ({ children }) => {
   const [results, setResults] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [searchTerm, setSearchTerm] = useState("Elon Musk");
+  const [searchTerm, setSearchTerm] = useState("Github");
 
   // check youtube url
   const matchYoutubeUrl = (url) => {
